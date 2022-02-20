298999cf08922c02b07560c5a5c6bda4658c76b1
Nikita Fomin <97741120+r1xelraze@users.noreply.github.com>
2022-02-20 15:20:06 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        double summ=0;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите n: ");
+        int n = scan.nextInt();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Введите x: ");
+        int x = scan2.nextInt();
+        for (int i = 1; i<n; i++){
+            int p = (2*i-1);
+            int w = (2*i*(2 * i + 1));
+            double q = Math.pow(x,(2*i+1) );
+            double task = ((p * q) / w);
+            summ = summ + task;
+        }
+        System.out.println("Результат: ");
+        System.out.println(x+summ);
+    }
+}
