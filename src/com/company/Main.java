479ad1cea2682a06b7b1e2e2202d3e5f025e80fc
Nikita Fomin <97741120+r1xelraze@users.noreply.github.com>
2022-02-25 10:42:29 +0300

    Конечное изменение

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         double summ=0;
-
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите n: ");
         int n = scan.nextInt();
-        Scanner scan2 = new Scanner(System.in);
+        if (n<1){
+            System.out.println("Введите число соответствующее условию");
+            return;
+        }
         System.out.println("Введите x: ");
-        int x = scan2.nextInt();
+        int x = scan.nextInt();
         for (int i = 1; i<n; i++){
             int p = (2*i-1);
             int w = (2*i*(2 * i + 1));
